4882e7ac8c960c508c867764f8b2e41ea9464735
dar-shan-73 <naik.darshan73@gmail.com>
2025-02-22 23:07:48 +0530

    frontend file with if conditions added

--- 001-frontend.sh
+++ 001-frontend.sh
@@ -8,17 +8,28 @@ if [ "$ID" -ne 0 ] ; then
     exit 1
 fi
 
-
+echo "Installing nginx"
 dnf install nginx -y
+
+echo "copying proxy file"
 cp proxy.conf /etc/nginx/default.d/expense.conf
 
+echo "enabling nginx"
 systemctl enable nginx
+
+echo "performing a cleanup"
 rm -rf /usr/share/nginx/html/*
+
+echo "downloading frontend"
 curl -o /tmp/frontend.zip https://expense-web-app.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
+
+echo "extracting frontend"
 unzip /tmp/frontend.zip
 pwd
 ls -ltr
+
+echo "starting frontend"
 systemctl restart nginx
 
 echo "*frontend installation is completed*"
