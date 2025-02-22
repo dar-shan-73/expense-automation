6779d988802cb28e15a42bca18ee5bff84c1af9d
dar-shan-73 <naik.darshan73@gmail.com>
2025-02-22 22:59:57 +0530

    frontend file with if conditions added

--- 001-frontend.sh
+++ 001-frontend.sh
@@ -3,7 +3,7 @@
 ID=$(id -u)
 
 if [ "$ID" -ne 0 ] ; then
-    echo -e "\e[31m script is expected to execute as root user \e[0m"
+    echo -e "\e[31m script is expected to be executed as root user \e[0m"
     exit 1
 fi
 
