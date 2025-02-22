ff807619493fb8692dd78772cd5a0fe7c3e714bd
dar-shan-73 <naik.darshan73@gmail.com>
2025-02-22 23:36:42 +0530

    frontend file with if conditions added

--- 001-frontend.sh
+++ 001-frontend.sh
@@ -12,30 +12,61 @@ echo "Installing nginx"
 dnf install nginx -y &>> /tmp/frontend.log
 
 if [ $? -eq 0 ] ; then
-    echo -e "\e[31m --success-- \e[0m"
+    echo -e "\e[32m --success-- \e[0m"
+else 
+    echo  -e "\e31m --failure-- \e[0m"
+fi       
 
 echo "copying proxy file"
 cp proxy.conf /etc/nginx/default.d/expense.conf &>> /tmp/frontend.log
 
 if [$? -eq 0 ] ; then
     echo -e "\e[31m --success-- \e[0m"
+else 
+    echo  -e "\e31m --failure-- \e[0m"
+fi      
 
 echo "enabling nginx"
 systemctl enable nginx &>> /tmp/frontend.log
+if [$? -eq 0 ] ; then
+    echo -e "\e[31m --success-- \e[0m"
+else 
+    echo  -e "\e31m --failure-- \e[0m"
+fi  
 
 echo "performing a cleanup"
 rm -rf /usr/share/nginx/html/* &>> /tmp/frontend.log
+if [$? -eq 0 ] ; then
+    echo -e "\e[31m --success-- \e[0m"
+else 
+    echo  -e "\e31m --failure-- \e[0m"
+fi  
 
 echo "downloading frontend"
 curl -o /tmp/frontend.zip https://expense-web-app.s3.amazonaws.com/frontend.zip &>> /tmp/frontend.log
+if [$? -eq 0 ] ; then
+    echo -e "\e[31m --success-- \e[0m"
+else 
+    echo  -e "\e31m --failure-- \e[0m"
+fi  
 cd /usr/share/nginx/html
 
 echo "extracting frontend"
 unzip /tmp/frontend.zip &>> /tmp/frontend.log
+if [$? -eq 0 ] ; then
+    echo -e "\e[31m --success-- \e[0m"
+else 
+    echo  -e "\e31m --failure-- \e[0m"
+fi  
 pwd &>> /tmp/frontend.log
 ls -ltr &>> /tmp/frontend.log
 
 echo "starting frontend"
 systemctl restart nginx &>> /tmp/frontend.log
+if [$? -eq 0 ] ; then
+    echo -e "\e[31m --success-- \e[0m"
+else 
+    echo  -e "\e31m --failure-- \e[0m"
+fi  
 
 echo "*frontend installation is completed*"
